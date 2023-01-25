1afb84999975b84a5bb5cbed32dfc67049e31eea
hamcoding9 <dientes8520@gmail.com>
2023-01-25 22:30:54 +0900

    [build] 기본 ui 생성, build 설정

--- app/src/main/java/com/example/screendetox2/ui/HomeActivity.kt
+++ app/src/main/java/com/example/screendetox2/ui/HomeActivity.kt
@@ -1,9 +1,10 @@
-package com.example.screendetox2
+package com.example.screendetox2.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.screendetox2.R
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
