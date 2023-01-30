fd77663fbc031f4f7dc9781d8eaebbcf492388f3
hamcoding9 <dientes8520@gmail.com>
2023-01-30 16:23:35 +0900

    [fix] 빌드 오류 수정

--- app/src/main/java/com/hamcoding/screendetox/ui/stats/StatsFragment.kt
+++ app/src/main/java/com/hamcoding/screendetox/ui/stats/StatsFragment.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.screendetox2.R
+import com.hamcoding.screendetox.R
 
 class StatsFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
