38b96a7ebea6a5bad5597fb305caf1f6a76cc848
hamcoding9 <dientes8520@gmail.com>
2023-01-30 19:45:39 +0900

    [feat] jetpack navigation으로 bottom navigation 연결

--- app/src/main/java/com/hamcoding/screendetox/ui/HomeActivity.kt
+++ app/src/main/java/com/hamcoding/screendetox/ui/HomeActivity.kt
@@ -4,12 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.hamcoding.screendetox.R
+import com.hamcoding.screendetox.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 
     override fun onStart() {
