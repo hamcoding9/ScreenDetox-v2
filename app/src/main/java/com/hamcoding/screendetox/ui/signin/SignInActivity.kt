88d05057a1a0dffdb5c079c18bd2e4c84ef9127d
hamcoding9 <dientes8520@gmail.com>
2023-02-04 22:24:01 +0900

    [feat] 구글 로그인 layout 추가

---
+++ app/src/main/java/com/hamcoding/screendetox/ui/signin/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.hamcoding.screendetox.ui.signin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hamcoding.screendetox.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
