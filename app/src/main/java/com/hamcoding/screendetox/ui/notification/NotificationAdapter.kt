5ae400664a07eabd537038e3b5404eab1a15d33b
hamcoding9 <dientes8520@gmail.com>
2023-02-07 18:21:37 +0900

    [feat] notification 화면 임시 구현

---
+++ app/src/main/java/com/hamcoding/screendetox/ui/notification/NotificationAdapter.kt
@@ -0,0 +1,45 @@
+package com.hamcoding.screendetox.ui.notification
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.hamcoding.screendetox.data.App
+import com.hamcoding.screendetox.databinding.ItemNotificationBinding
+import com.hamcoding.screendetox.databinding.ItemStatsBinding
+import com.hamcoding.screendetox.ui.stats.AppAdapter.Companion.diffUtil
+
+class NotificationAdapter() : ListAdapter<String, NotificationAdapter.ViewHolder>(diffUtil) {
+
+    class ViewHolder(private val binding: ItemNotificationBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(data: String) {
+            binding.tvInfo.text = data
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return ViewHolder(ItemNotificationBinding.inflate(inflater, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<String>() {
+            override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+                return oldItem.hashCode() == newItem.hashCode()
+            }
+
+            override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+
+}
